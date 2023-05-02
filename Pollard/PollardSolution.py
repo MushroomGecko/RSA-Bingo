1e3926270162d849bb6bb0e02ac13e5735e2734e
MushroomGecko <70172740+MushroomGecko@users.noreply.github.com>
2023-05-02 14:11:15 -0400

    Updated solutions

--- Pollard/PollardSolution.py
+++ Pollard/PollardSolution.py
@@ -7,7 +7,7 @@ import time
 from timeit import default_timer as timer
 
 
-# Created from pseudocode in Professor's notes
+# Created from pseudocode in Professor Marron's notes
 # takes in an n value and a seed and finds powmods and gcds of y-x and n until variable d is no longer 1 or n
 def PollardRho(num, n, rand):
     start = timer()
@@ -26,6 +26,42 @@ def PollardRho(num, n, rand):
         if i == k:
             y = x
             k = k * 2
+
+def PollardRho2(num, n, rand):
+    start = timer()
+    i = 1
+    x = gmpy2.mpz_random(gmpy2.random_state(rand), n - 1)
+    y = x
+    k = 2
+    while True:
+        i = i + 1
+        x = (pow(x, 2) - 1) % n
+        d = gmpy2.gcd(y - x, n)
+        if d != 1 and d != n:
+            print("Rho 2 Thread:", num, d)
+            print("Rho 2 Time (s):", timer() - start)
+            return 1
+        if i == k:
+            y = x
+            k = k * 2
+
+def g(i, n):
+    return (pow(i+1, 2, n))
+
+def PollardRhoRJ(num, n):
+    start = timer()
+    x = 2
+    y = 2
+    p = 1
+    while p == 1:
+        x = g(x, n)
+        y = g(g(y, n), n)
+        p = gmpy2.gcd(abs(x - y), n)
+    if p == n:
+        return -1
+    else:
+        print("Rho RJ Thread:", num, p)
+        print("Rho RJ Time (s):", timer() - start)
 
 # Inspired from Geeks For Geeks, Purdue, untruth.org, and a github project (Links to all below)
 # Takes in a thread number and an n value. a starts as 2 and B starts as 2. a is the powmod of itself to the B power mod n. g then uses a to fnd the gcd of a-1 and n to see if a factored occurred.
@@ -51,6 +87,23 @@ def PollardRhoMulti(num_processes, num):
         t1.start()
         time_var = time_var + 10000
 
+# Meant to invoke multiprocessing for Pollard Rho
+def PollardRho2Multi(num_processes, num):
+    time_var = int(time.time())
+    processes = num_processes
+    for i in range(1, processes + 1):
+        t1 = multiprocessing.Process(target=PollardRho2, args=(i, num, time_var,))
+        t1.start()
+        time_var = time_var + 10000
+
+def PollardRhoRJMulti(num_processes, num):
+    # time_var = int(time.time())
+    processes = num_processes
+    for i in range(1, processes + 1):
+        t1 = multiprocessing.Process(target=PollardRhoRJ, args=(i, num))
+        t1.start()
+        # time_var = time_var + 10000
+
 
 # Meant to invoke multiprocessing for Pollard P-1 (inspired sources version)
 def PollardP1Multi(num_processes, num):
@@ -67,14 +120,20 @@ if __name__ == "__main__":
     parser.add_argument("--threads", type=int, required=False, default=1, help="Number of threads to run")
     parser.add_argument("--do-rho", type=int, required=False, default=1, help="Activate Pollard's Rho. 0 to deactivate, 1 to activate (defaulted to 1)")
     parser.add_argument("--do-p1", type=int, required=False, default=1, help="Activate Pollard's P-1. 0 to deactivate, 1 to activate (defaulted to 1)")
+    parser.add_argument("--do-rj", type=int, required=False, default=1, help="Activate Pollard's P-1. 0 to deactivate, 1 to activate (defaulted to 1)")
+    parser.add_argument("--do-rho2", type=int, required=False, default=1, help="Activate Pollard's P-1. 0 to deactivate, 1 to activate (defaulted to 1)")
     args = parser.parse_args()
 
     factor = mpz(args.n)
 
+    if args.do_rho2 > 0:
+        PollardRho2Multi(args.threads, factor)
     if args.do_rho > 0:
         PollardRhoMulti(args.threads, factor)
     if args.do_p1 > 0:
         PollardP1Multi(args.threads, factor)
+    if args.do_rj > 0:
+        PollardRhoRJMulti(args.threads, factor)
 
 # Links to sites that inspired my Pollard's P-1
 # https://www.geeksforgeeks.org/pollard-p-1-algorithm/
--- Pollard/PollardTimeTesting.py
+++ Pollard/PollardTimeTesting.py
@@ -7,7 +7,7 @@ import time
 from timeit import default_timer as timer
 
 ########## Pollard Rho Testing ##########
-def PollardRhoBase(n, rand):
+def PollardRhoBase(n, rand, thread):
     start = timer()
     i = 1
     x = gmpy2.mpz_random(gmpy2.random_state(rand), n-1)
@@ -18,12 +18,13 @@ def PollardRhoBase(n, rand):
         x = gmpy2.powmod((pow(x, 2)-1), 1, n)
         d = gmpy2.gcd(y-x, n)
         if d != 1 and d != n:
+            print("Pollard Base p thread " + str(thread) + ": " + str(d))
             return timer() - start
         if i == k:
             y = x
             k = k * 2
 
-def PollardRhoMPZ(n, rand):
+def PollardRhoMPZ(n, rand, thread):
     start = timer()
     i = mpz(1)
     x = gmpy2.mpz_random(gmpy2.random_state(rand), n-1)
@@ -34,13 +35,14 @@ def PollardRhoMPZ(n, rand):
         x = gmpy2.powmod((pow(x, 2)-1), 1, n)
         d = gmpy2.gcd(gmpy2.sub(y, x), n)
         if d != 1 and d != n:
+            print("Pollard MPZ p thread " + str(thread) + ": " + str(d))
             return timer() - start
         if i == k:
             y = x
             k = gmpy2.mul(mpz(k), 2)
 
 ########## PollardP1 Testing ##########
-def PollardP1Base(n):
+def PollardP1Base(n, thread):
     start = timer()
     a = 2
     B = 2
@@ -48,10 +50,11 @@ def PollardP1Base(n):
         a = gmpy2.powmod(a, B, n)
         g = gmpy2.gcd((a-1), n)
         if g != 1 and g != n:
+            print("P1 Base p thread " + str(thread) + ": " + str(g))
             return timer() - start
         B += 1
 
-def PollardP1MPZ(n):
+def PollardP1MPZ(n, thread):
     start = timer()
     a = mpz(2)
     B = mpz(2)
@@ -59,6 +62,7 @@ def PollardP1MPZ(n):
         a = gmpy2.powmod(mpz(a), B, n)
         g = gmpy2.gcd(mpz(a-1), n)
         if g != 1 and g != n:
+            print("P1 MPZ p thread " + str(thread) + ": " + str(g))
             return timer() - start
         B = gmpy2.add(B, 1)
 
@@ -67,9 +71,11 @@ def PollardRhoBaseMulti(num_processes, size):
     time_var = int(time.time())
     processes = num_processes
     t1 = 0
-    for i in range(1,processes+1):
+    i = 0
+    for i in range(1, processes+1):
         (pubkey, privkey) = rsa.newkeys(size)
-        t1 = t1 + PollardRhoBase(privkey.n, time_var)
+        print("Pollard Base n thread " + str(i) + ": " + str(privkey.n))
+        t1 = t1 + PollardRhoBase(privkey.n, time_var, i)
         time_var = time_var + 10000
     print("Pollard Base Avg:", t1/i)
 
@@ -77,9 +83,11 @@ def PollardRhoMPZMulti(num_processes, size):
     time_var = int(time.time())
     processes = num_processes
     t1 = 0
-    for i in range(1,processes+1):
+    i = 0
+    for i in range(1, processes+1):
         (pubkey, privkey) = rsa.newkeys(size)
-        t1 = t1 + PollardRhoMPZ(privkey.n, time_var)
+        print("Pollard MPZ n thread " + str(i) + ": " + str(privkey.n))
+        t1 = t1 + PollardRhoMPZ(privkey.n, time_var, i)
         time_var = time_var + 10000
     print("Pollard MPZ Avg:", t1/i)
 
@@ -87,9 +95,11 @@ def PollardP1BaseMulti(num_processes, size):
     time_var = int(time.time())
     processes = num_processes
     t1 = 0
-    for i in range(1,processes+1):
+    i = 0
+    for i in range(1, processes+1):
         (pubkey, privkey) = rsa.newkeys(size)
-        t1 = t1 + PollardP1Base(privkey.n)
+        print("P1 Base n thread " + str(i) + ": " + str(privkey.n))
+        t1 = t1 + PollardP1Base(privkey.n, i)
         time_var = time_var + 10000
     print("P1 Base Avg:", t1/i)
 
@@ -97,9 +107,11 @@ def PollardP1MPZMulti(num_processes, size):
     time_var = int(time.time())
     processes = num_processes
     t1 = 0
-    for i in range(1,processes+1):
-        (pubkey, privkey) = rsa.newkeys(args.rsa_size)
-        t1 = t1 + PollardP1MPZ(privkey.n)
+    i = 0
+    for i in range(1, processes+1):
+        (pubkey, privkey) = rsa.newkeys(size)
+        print("P1 MPZ n thread " + str(i) + ": " + str(privkey.n))
+        t1 = t1 + PollardP1MPZ(privkey.n, i)
         time_var = time_var + 10000
     print("P1 MPZ Avg:", t1/i)
 
